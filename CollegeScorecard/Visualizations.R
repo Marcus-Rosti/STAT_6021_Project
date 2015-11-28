8b7b8bedf09acc0a57956fd1287813b464d38a97
Michael Voltmer <m.voltmer@gmail.com>
2015-11-28 18:00:41 -0500

    Debt Map
    Added US shapefiles, code to create median debt map, and the map png

--- CollegeScorecard/Visualizations.R
+++ CollegeScorecard/Visualizations.R
@@ -20,3 +20,56 @@ plot(DEBT_MDN~COSTT4_A,data=college_df)
 plot(DEBT_MDN~TUITIONFEE_IN,data=college_df)
 
 plot(DEBT_MDN~TUITIONFEE_OUT,data=college_df)
+
+######################
+# PLOT DEBT BY STATE #
+######################
+
+require("rgdal")
+require("maptools")
+require("ggplot2")
+require("plyr")
+require("RColorBrewer")
+require("scales")
+
+# read in placement data
+data <- read.csv("MERGED2013_PP.csv", na.strings=c("NULL", "PrivacySuppressed"), stringsAsFactors = FALSE)
+
+# import shape file
+state <- readOGR(dsn = "us_shapefiles", layer = "tl_2010_us_state10")
+state@data$id <- rownames(state@data)
+
+# convert polygons in state to a data frame for plotting
+state.df <- fortify(state)
+
+# calculate average salaries
+avg.debt <- aggregate(DEBT_MDN ~ STABBR, data = data, FUN = mean)
+colnames(avg.debt)[1] <- "STUSPS10"
+# remove "MP"
+avg.debt <- avg.debt[-26, ]
+
+# join columns
+state.df <- join(state.df, state@data, by="id")
+state.df <- join(state.df, avg.debt, by = "STUSPS10")
+
+state.df <- state.df[state.df$long < 0,] # delete random rows with inaccurate longitudes
+
+# remove Puerto Rico, Alaska, and Hawaii
+state.df <- state.df[state.df$NAME10 != "Puerto Rico",]
+state.df <- state.df[state.df$NAME10 != "Alaska",]
+state.df <- state.df[state.df$NAME10 != "Hawaii",]
+
+# plot by count of students per state
+ggp <- ggplot(data=state.df, aes(x=long, y=lat, group=group))   # draw polygons
+ggp <- ggp + geom_path(color="#d3d3d3", size = 1) # draw boundaries
+ggp <- ggp + coord_map()
+ggp <- ggp + scale_fill_gradient(limits=c(8243, 15988), low="#c6dbef", high = "#08306b", na.value = "white")
+ggp <- ggp + geom_polygon(aes(fill = DEBT_MDN))
+ggp <- ggp + ggtitle(expression(atop(bold("2013 Median Debt"), atop("By State", ""))))
+ggp <- ggp + theme(panel.background = element_blank(),
+                   axis.ticks = element_blank(),
+                   axis.text = element_blank(),
+                   axis.title = element_blank(), 
+                   panel.grid = element_blank())
+
+plot(ggp)
